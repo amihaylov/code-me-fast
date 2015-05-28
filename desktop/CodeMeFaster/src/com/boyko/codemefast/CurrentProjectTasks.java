0b693570a0fffd09c6b8037f11d474c0cbe95dad
boyskila <bmlalov@abv.bg>
2015-05-28 19:07:13 +0300

    UI

---
+++ desktop/CodeMeFaster/src/com/boyko/codemefast/CurrentProjectTasks.java
@@ -0,0 +1,13 @@
+package com.boyko.codemefast;
+
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+public class CurrentProjectTasks extends JPanel {
+    public CurrentProjectTasks() {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setBackground(Color.PINK);
+    }
+}
